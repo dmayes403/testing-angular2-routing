b90be04c881ee1bdf95fd42696dcb8583ea73fff
David Mayes <I21569@xactware.com>
2017-06-07 10:21:13 -0600

    finished routing practice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,23 +10,32 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { ServerResolver } from './servers/server/server-resolver.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent, children: [
     { path: ':id/:name', component: UserComponent },
   ] },
-  { path: 'servers', canActivate: [AuthGuard], component: ServersComponent, children: [
-    { path: ':id', component: ServerComponent },
-    { path: ':id/edit', component: EditServerComponent },
+  { path: 'servers',
+    // canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    component: ServersComponent,
+    children: [
+    { path: ':id', component: ServerComponent, resolve: {server: ServerResolver} },
+    { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] },
   ] },
-  { path: 'not-found', component: PageNotFoundComponent },
+  // { path: 'not-found', component: PageNotFoundComponent },
+  { path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found!'} },
   { path: '**', redirectTo: '/not-found', pathMatch: 'full' } // <-- should be the very last route!
 
 ];
 
 @NgModule({
   imports: [
+    // RouterModule.forRoot(appRoutes, {useHash: true})  // <--- adding hash is for older browsers
     RouterModule.forRoot(appRoutes)
   ],
   exports: [RouterModule]
--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router, Data } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -16,15 +16,21 @@ export class ServerComponent implements OnInit {
               private router: Router) { }
 
   ngOnInit() {
-    const id = +this.route.snapshot.queryParams['id']
-    console.log(this.route.snapshot.queryParams);
-    this.server = this.serversService.getServer(id);
-    this.route.params
+    this.route.data
       .subscribe(
-        (params: Params) => {
-          this.server = this.serversService.getServer(+params['id'])
+        (data: Data) => {
+          this.server = data['server']
         }
-      )
+      );
+    // const id = +this.route.snapshot.queryParams['id']
+    // console.log(this.route.snapshot.queryParams);
+    // this.server = this.serversService.getServer(id);
+    // this.route.params
+    //   .subscribe(
+    //     (params: Params) => {
+    //       this.server = this.serversService.getServer(+params['id'])
+    //     }
+    //   )
   }
 
   onEdit(){
